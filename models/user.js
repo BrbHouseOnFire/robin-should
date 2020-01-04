489396e423d3891538d57d986368781724eb65f1
Brb House on Fire <bobbywoj@sbcglobal.net>
2020-01-04 10:58:26 -0600

    removing vars

--- models/user.js
+++ models/user.js
@@ -1,29 +1,29 @@
 // Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
+const orm = require("../config/orm.js");
 
-// var burger = {
+// let user = {
 //   all: function(cb) {
-//     orm.all("burgers", function(res) {
+//     orm.all("users", function(res) {
 //       cb(res);
 //     });
 //   },
 //   // The variables cols and vals are arrays.
 //   create: function(cols, vals, cb) {
-//     orm.create("burgers", cols, vals, function(res) {
+//     orm.create("users", cols, vals, function(res) {
 //       cb(res);
 //     });
 //   },
 //   update: function(objColVals, condition, cb) {
-//     orm.update("burgers", objColVals, condition, function(res) {
+//     orm.update("users", objColVals, condition, function(res) {
 //       cb(res);
 //     });
 //   },
 //   delete: function(condition, cb) {
-//     orm.delete("burgers", condition, function(res) {
+//     orm.delete("users", condition, function(res) {
 //       cb(res);
 //     });
 //   }
 // };
 
-// Export the database functions for the controller (burgersController.js).
+// Export the database functions for the controller (usersController.js).
 module.exports = user;
