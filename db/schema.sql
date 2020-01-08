b091593368ec8bb62e27ea255ef54ca34ca126f6
gjpataky <gjpataky@yahoo.com>
2020-01-07 19:10:17 -0600

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,52 @@
 CREATE DATABASE robin_db;
 USE robin_db;
 
-CREATE TABLE expenses
+CREATE TABLE expenses_budgeted
 (
-	id int NOT NULL AUTO_INCREMENT,
-	user varchar(255) NOT NULL,
-	amount DECIMAL(63,2) NOT NULL,
+	id int NOT NULL
+	AUTO_INCREMENT,
+	user varchar
+	(255) NOT NULL,
+	amount DECIMAL
+	(63,2) NOT NULL,
 	category_id int NOT NULL,
     creation_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (id)
+	PRIMARY KEY
+	(id)
 );
-CREATE TABLE income
-(
-	id int NOT NULL AUTO_INCREMENT,
-	user varchar(255) NOT NULL,
-	amount DECIMAL(63,2) NOT NULL,
+
+	CREATE TABLE expenses_actual
+	(
+		id int NOT NULL
+		AUTO_INCREMENT,
+	user varchar
+		(255) NOT NULL,
+	amount DECIMAL
+		(63,2) NOT NULL,
+	category_id int NOT NULL,
     creation_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (id)
+	PRIMARY KEY
+		(id)
 );
-CREATE TABLE categories
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
+		CREATE TABLE income
+		(
+			id int NOT NULL
+			AUTO_INCREMENT,
+	user varchar
+			(255) NOT NULL,
+	amount DECIMAL
+			(63,2) NOT NULL,
     creation_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (id)
+	PRIMARY KEY
+			(id)
+);
+			CREATE TABLE categories
+			(
+				id int NOT NULL
+				AUTO_INCREMENT,
+	name varchar
+				(255) NOT NULL,
+    creation_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY
+				(id)
 );
