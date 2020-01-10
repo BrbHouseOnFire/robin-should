791b858524f37d0f870e13e52a309eb59fdd94af
gjpataky <gjpataky@yahoo.com>
2020-01-09 21:03:48 -0600

    profile

--- public/assets/js/front-end-function.js
+++ public/assets/js/front-end-function.js
@@ -2,9 +2,9 @@
 
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  // $(".change-sleep").on("click", function(event) {
-  //   var id = $(this).data("id");
-  //   var newSleep = $(this).data("newsleep");
+   $(".change-sleep").on("click", function(event) {
+    var id = $(this).data("id");
+    var newSleep = $(this).data("newsleep");
 
   //   var newSleepState = {
   //     sleepy: newSleep
