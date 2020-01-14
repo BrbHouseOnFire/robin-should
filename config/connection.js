07b86c466f25d3f73efde288ac1c80bea0e91816
Brb House on Fire <bobbywoj@sbcglobal.net>
2020-01-14 16:04:20 -0600

    connection update for JAWSDB on heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
-
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "robin_db"
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "robin_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
