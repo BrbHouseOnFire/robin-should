9389a1dee958d21031655cc7abfa1a55742da1fd
Brb House on Fire <bobbywoj@sbcglobal.net>
2020-01-13 20:31:03 -0600

    cleanup frontend JS

--- public/assets/js/front-end-function.js
+++ public/assets/js/front-end-function.js
@@ -71,73 +71,7 @@ $(document).ready(function(){
 
 
 
-$(function() {
-  let profileName = document.getElementById("userName");
-  let profileIncome = document.getElementById("monthlyIncome");
-  let profile = document.getElementById("createProfile");
-  document.body.addEventListener("click", function(evt) {
-      if (evt.target.className === "profButtons text-heebo text-heebo mt-2 mb-2") {
-        alert();
-      }
-  });
 
-  /*
-  profile.addEventListener('click', function () {
-    alert(profileName.value + profileIncome.value);
-    console.log(profileName.value);
-
-  });*/
-
-  // $("#home").click(function() {
-  //   window.location.href = "/lifestyle/1/:username";
-  // });
-
-  // $("#budget").click(function() {
-  //   window.location.href = "/budget/set/1/:username";
-  // });
-
-  // $("#expenses").click(function() {
-  //   window.location.href = "/budget/expenses/:username";
-  // });
-
-
-
-
-  var selectedCategory = "";
-
-  $("#selectCategory").on("change", function(e) {
-    var optionSelected = $("option:selected", this);
-    selectedCategory = this.value;
-    console.log("new category: " + selectedCategory);
-  });
-
-  $("#submitExpenses").click(function() {
-    var expenseVal = $("#expense").val();
-    var userName = $("#userName").text();
-    var newCategory = {
-      category_id: selectedCategory,
-      user: userName,
-      amount: expenseVal
-    };
-    console.log(newCategory);
-
-    var jsonData = JSON.stringify(newCategory);
-
-    $.ajax({
-      url: "api/add/expenses",
-      type: "POST",
-      contentType: "application/json; charset=utf-8",
-      data: jsonData,
-      dataType: "json",
-      success: function(data) {
-        console.log(data);
-      },
-      error: function(error) {
-        console.log(error);
-      }
-    });
-  });
-});
 
 
 
